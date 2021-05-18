e8401cb8ca345c861b3d6dfa5730e64b37b09a1a
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-05-18 14:53:57 +0300

    generated card input component

--- Moringa_Projects/LORNAS QUOTES APP/quotes/src/app/app.module.ts
+++ Moringa_Projects/LORNAS QUOTES APP/quotes/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormInputComponent } from './form-input/form-input.component';
+import { CardInputComponent } from './card-input/card-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormInputComponent
+    FormInputComponent,
+    CardInputComponent
   ],
   imports: [
     BrowserModule,
